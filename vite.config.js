e203b5aa4ff9f85fdf0b04032e3655a14f7b6b09
randhayoga <92174528+randhayoga@users.noreply.github.com>
2026-05-08 10:22:54 +0700

    feat: enable HTTPS in development server using vite-plugin-basic-ssl

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
     plugins: [
+        basicSsl(),
         laravel({
             input:
             'resources/js/app.ts',
@@ -23,9 +25,11 @@ export default defineConfig({
     ],
     server: {
         host: '0.0.0.0',
+        https: true,
         cors: true,
         hmr: {
-            host: '127.0.0.1',
+            host: 'localhost',
+            protocol: 'wss',
         }
     },
 });
